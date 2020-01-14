e053d9e647ca699acfe3d1e9e028260ab9768c6f
audreybichelmeir <bichelmeir@chapman.edu>
2020-01-14 21:03:29 +0000

    edits

--- newHelloWorld.cpp
+++ newHelloWorld.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main (int argc, char **argv)
 {
 cout << "hello world" << endl;
-cout << "to add this statement cause Rene said so";
-cout << "another cout" <<;
+cout << "add this statement cause Rene said so";
+cout << "another cout";
 
 return 0;
 }
