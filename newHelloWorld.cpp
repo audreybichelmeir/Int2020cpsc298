bd9b63a959b37b9e93806e87f9d3074b1278c34b
audreybichelmeir <bichelmeir@chapman.edu>
2020-01-14 20:56:57 +0000

    New change

--- newHelloWorld.cpp
+++ newHelloWorld.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main (int argc, char **argv)
 {
 cout << "hello world" << endl;
+cout << "to add this statement cause Rene said so";
+cout << "another cout" <<;
 
 return 0;
 }
